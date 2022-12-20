7e2ebf229b5ff11f8de02ad63c063c4776b16fc7
kuro-vale <jsalcedo218@gmail.com>
2022-12-20 16:22:39 -0500

    Remove unused dependency

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { Schema } from "./schema.js";
 import { Resolvers } from "./resolvers.js";
 import { PrismaClient } from "@prisma/client";
-import depthLimit from "graphql-depth-limit";
 
 const prisma = new PrismaClient();
 
 const server = new ApolloServer({
   typeDefs: Schema,
   resolvers: Resolvers,
-  validationRules: [depthLimit(3)],
 });
 
 const { url } = await startStandaloneServer(server, {
